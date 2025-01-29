475fc8cc8113f92d479c0d56da7fdcac801fb7c7
Timur Vlasov <tim.vlasov9@gmail.com>
2025-01-29 16:44:41 +0000

    added tests

---
+++ src/test/java/tests/PracticeFormTest.java
@@ -0,0 +1,74 @@
+package tests;
+
+import components.UserTableComponent;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationPage;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeFormTest extends BaseTest {
+
+    RegistrationPage registrationPage = new RegistrationPage();
+
+    @Test
+    void successfulFillingFormTest() {
+
+        registrationPage.openPage()
+                .setName("Timur")
+                .setLastName("Vlasov")
+                .setUserEmail("test@email.com")
+                .setUserGender("Male")
+                .setUserNumber("7941643176")
+                .setUserBirthday("03", "October", "1996")
+                .setUserSubjects("English").setUserHobbies("Music")
+                .uploadUserPicture("sample-pic.jpg")
+                .setUserAddress("г. Барнаул, ул. Гоголя, д. 38")
+                .setUserState("Haryana")
+                .setUserCity("Karnal")
+                .submitRegistrationForm()
+                .checkUserData("Student Name", "Timur Vlasov")
+                .checkUserData("Student Email", "test@email.com")
+                .checkUserData("Gender", "Male")
+                .checkUserData("Mobile", "7941643176")
+                .checkUserData("Date of Birth", "03 October,1996")
+                .checkUserData("Subjects", "English")
+                .checkUserData("Hobbies", "Music")
+                .checkUserData("Picture", "sample-pic.jpg")
+                .checkUserData("Address", "г. Барнаул, ул. Гоголя, д. 38")
+                .checkUserData("State and City", "Haryana Karnal");
+    }
+
+    @Test
+    void fillFormWithNecessaryFieldsTest() {
+
+        registrationPage.openPage()
+                .setName("Timur")
+                .setLastName("Vlasov")
+                .setUserGender("Male")
+                .setUserNumber("7941643176")
+                .submitRegistrationForm()
+                .checkUserData("Student Name", "Timur Vlasov")
+                .checkUserData("Gender", "Male")
+                .checkUserData("Mobile", "7941643176");
+
+    }
+
+    @Test
+    void fillFormWithoutNecessaryFieldsTest() {
+
+        registrationPage.openPage()
+                .setUserEmail("test@email.com")
+                .setUserBirthday("03", "October", "1996")
+                .setUserSubjects("English").setUserHobbies("Music")
+                .uploadUserPicture("sample-pic.jpg")
+                .setUserAddress("г. Барнаул, ул. Гоголя, д. 38")
+                .setUserState("Haryana")
+                .setUserCity("Karnal")
+                .submitRegistrationForm()
+                .checkTableExist();
+    }
+    
+}
